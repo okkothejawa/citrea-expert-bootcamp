75c9c960d700131f01ad48caac7c9f5182056df2
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-07-23 03:11:03 +0300

    contracts

--- src/BitcoinMinerIncentives.sol
+++ src/BitcoinMinerIncentives.sol
@@ -3,16 +3,63 @@ pragma solidity ^0.8.13;
 
 import "./IBitcoinLightClient.sol";
 import "bitcoin-spv/solidity/contracts/ValidateSPV.sol";
+import "bitcoin-spv/solidity/contracts/BTCUtils.sol";
+import "bitcoin-spv/solidity/contracts/CheckBitcoinSigs.sol";
+
 
 contract BitcoinMinerIncentives {
+    using BTCUtils for bytes;
+    struct BtcTxn {
+        bytes4 version;
+        bytes vin;
+        bytes vout;
+        bytes4 locktime;
+        bytes intermediateNodes;
+        uint256 index;
+    }
+
+    uint256 rewardAmount = 0.01 ether;
+
+    mapping(uint256 => bool) public claimed;
+
     receive() external payable {}
 
     IBitcoinLightClient public lightClient = IBitcoinLightClient(address(0x3100000000000000000000000000000000000001));
 
-    function incentivizeMiner(uint256 blockHeight, uint256 reward, bytes memory blockHeader, bytes memory minerPubkey) public payable {
+    function incentivizeMiner(uint256 blockHeight, bytes memory blockHeader, bytes memory minerPubkey, BtcTxn memory coinbaseTxn) public {
+        require(!claimed[blockHeight], "Reward already claimed");
+        claimed[blockHeight] = true;
+
+        bytes32 providedBlockHash = blockHeader.hash256();
+        bytes32 trueBlockHash = lightClient.getBlockHash(blockHeight);
+        require(providedBlockHash == trueBlockHash, "Provided block hash does not match the true block hash");
+
+        bytes32 txRoot = blockHeader.extractMerkleRootLE();
+        bytes32 coinbaseTxId = ValidateSPV.calculateTxId(coinbaseTxn.version, coinbaseTxn.vin, coinbaseTxn.vout, coinbaseTxn.locktime);
+        require(ValidateSPV.prove(coinbaseTxId, txRoot, coinbaseTxn.intermediateNodes, 0), "Coinbase transaction is not included in the block");
         
+        bytes memory output0 = coinbaseTxn.vout.extractOutputAtIndex(0);
+        bytes memory scriptHash = abi.encodePacked(hex"0014", output0.extractHash());
+        bytes memory btcScript = calculateBitcoinScript(minerPubkey);
+        require(keccak256(scriptHash) == keccak256(btcScript), "Miner pubkey does not match the pubkey in the coinbase transaction");
+
+        address minerEVMAddress = calculateEVMAddress(minerPubkey);
+        (bool success, ) = minerEVMAddress.call{value: rewardAmount}("");
+        require(success, "Failed to send reward to miner");
     }
 
-    function checkDifficulty(bytes memory blockHeader) internal {
+    function calculateBitcoinScript(bytes memory pubkey) internal view returns (bytes memory) {
+        return CheckBitcoinSigs.p2wpkhFromPubkey(pubkey);
+    }
+
+    function calculateEVMAddress(bytes memory pubkey) internal pure returns (address) {
+        bytes32 res1;
+        bytes32 res2;
+        assembly {
+            // Remove the first byte (0x03) from the pubkey
+            res1 := mload(add(add(pubkey, 32), 1))
+            res2 := mload(add(add(pubkey, 32), 33))
+        }
+        return CheckBitcoinSigs.accountFromPubkey(abi.encodePacked(res1, res2));
     }
 }
---
+++ test/BitcoinMinerIncentives.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {BitcoinMinerIncentives} from "../src/BitcoinMinerIncentives.sol";
+import "bitcoin-spv/solidity/contracts/ValidateSPV.sol";
+import "bitcoin-spv/solidity/contracts/CheckBitcoinSigs.sol";
+
+
+contract BitcoinMinerIncentivesTest is Test {
+    BitcoinMinerIncentives bmi;
+
+    function setUp() public {
+        bmi = new BitcoinMinerIncentives();
+    }
+
+    function testIncentive() public {
+        vm.deal(address(bmi), 0.01 ether);
+        bytes memory blockHeader = hex"0000002041e7d147af6a599d289b0e012383f4ebd77663f46547313ec9bb47021d00000075c3823ceb511ba78014706e3bfbf5e1d35c21ac1be74f28fe845fd265df87ffee2b9e66ae77031ec9a60e00";
+        bytes memory minerPubkey = hex"036783b5ca89d7845fa9874685593a980a982d5cdb0e63b80456de91f704b49858";
+        
+        bytes4 version = hex"02000000";
+        bytes memory vin = hex"010000000000000000000000000000000000000000000000000000000000000000ffffffff04034a6306feffffff";
+        bytes memory vout = hex"021ffd029500000000160014652be9203bf5b60d4c2205612edbe2d537c1be0c0000000000000000776a24aa21a9ed0be041774d1c61595182e721c5097876abc8e91ec0c4b80ecd80376029f165924c4fecc7daa2490047304402202f92cb50502c5caafcb868cd70da1ea299bc034214c6f73e4a522f2f7302ad4102203ed3cb706856eafed382dd8a75ad1650169024a4ae6e5a6574c9b5d18805e58c0100";
+        bytes4 locktime = hex"00000000";
+        bytes memory output0 = BTCUtils.extractOutputAtIndex(vout, 0);
+        bytes memory scriptHash = BTCUtils.extractHash(output0);
+        bytes memory btcScript = CheckBitcoinSigs.p2wpkhFromPubkey(minerPubkey);
+
+        bytes memory intermediateNodes = hex"db90907211d60c424f589f2617c2c96d17c0256d9df47e0dbe963621de23cdb461d902cb548c3dc301068725aae39c29d25f0d0a0bb491f60179837ede933a7f";
+        uint256 index = 0;
+
+        BitcoinMinerIncentives.BtcTxn memory coinbaseTxn = BitcoinMinerIncentives.BtcTxn(version, vin, vout, locktime, intermediateNodes, index);
+        bmi.incentivizeMiner(418634, blockHeader, minerPubkey, coinbaseTxn);
+    }
+}

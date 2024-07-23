355b43ba84efb3fd810d11b75a20096f513e4c09
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-07-23 11:04:16 +0300

    clean

--- test/BitcoinMinerIncentives.t.sol
+++ test/BitcoinMinerIncentives.t.sol
@@ -24,8 +24,6 @@ contract BitcoinMinerIncentivesTest is Test {
         bytes memory vout = hex"021ffd029500000000160014652be9203bf5b60d4c2205612edbe2d537c1be0c0000000000000000776a24aa21a9ed0be041774d1c61595182e721c5097876abc8e91ec0c4b80ecd80376029f165924c4fecc7daa2490047304402202f92cb50502c5caafcb868cd70da1ea299bc034214c6f73e4a522f2f7302ad4102203ed3cb706856eafed382dd8a75ad1650169024a4ae6e5a6574c9b5d18805e58c0100";
         bytes4 locktime = hex"00000000";
         bytes memory output0 = BTCUtils.extractOutputAtIndex(vout, 0);
-        bytes memory scriptHash = BTCUtils.extractHash(output0);
-        bytes memory btcScript = CheckBitcoinSigs.p2wpkhFromPubkey(minerPubkey);
 
         bytes memory intermediateNodes = hex"db90907211d60c424f589f2617c2c96d17c0256d9df47e0dbe963621de23cdb461d902cb548c3dc301068725aae39c29d25f0d0a0bb491f60179837ede933a7f";
         uint256 index = 0;
